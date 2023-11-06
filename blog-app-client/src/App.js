69578f4a9d2eadce999cd14a9b3031aa97de7c14
thisarakaushan <thisarak9992@gmail.com>
2023-11-06 23:15:51 +0530

    Add files to blog-app-client directory

--- blog-app-client/src/App.js
+++ blog-app-client/src/App.js
@@ -2,6 +2,9 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 // Pages
 import Home from "./pages/Home";
+import About from "./pages/About";
+import ArticlesList from "./pages/ArticlesList";
+import Article from "./pages/Article";
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <div className="max-w-screen-md mx-auto pt-20">
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/article-list' element={<ArticlesList />} />
+          <Route path='/article' element={<Article />} />
         </Routes>
     </div>
     </Router>
---
+++ blog-app-client/src/pages/About.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className="mb-20">
+            <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>
+                About
+            </h1>
+            <p className="mx-auto leading-relaxed text-base mb-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing etit. Rem esse,
+                iste consectetur totam sit et neque eos eius corrupt i. Accusantium quas
+                ea id assumenda it to repellendus quasi unde nam consectetur excepturi
+                dolorem eius repudiandae votuptates molestiae asperiores maxime ad maiores
+                modi commodi vetit nostrum cum, harum culpa aliquid? Quis, deserunt!
+            </p>
+            <p className="mx-auto leading-relaxed text-base mb-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing etit. Rem esse,
+                iste consectetur totam sit et neque eos eius corrupt i. Accusantium quas
+                ea id assumenda it to repellendus quasi unde nam consectetur excepturi
+                dolorem eius repudiandae votuptates molestiae asperiores maxime ad maiores
+                modi commodi vetit nostrum cum, harum culpa aliquid? Quis, deserunt!
+            </p>
+            <p className="mx-auto leading-relaxed text-base mb-4">
+                Lorem, ipsum dolor sit amet consectetur adipisicing etit. Earum
+                amet quibusdam, autem votuptatum odit porro reiciendis ipsam aperiam
+                sint ut distinctio nobis est recusandae quasi quos modi soluta illum
+                inventore, dolore sed accusantium quia voluptate. Ducimus, provident
+                veniam. Fugit, reiciendis aperiam veritatis nostrum, quisquam est
+                motestias animi suscipit pariatur assumenda corrupti perferendis,
+                molestiae magnam nesciunt perspiciatis commodi vero quibusdam id!
+                Expedita, incidunt. Culpa illo enim voluptates maxime voluptate, vet
+                nesciunt natus recusandae totam dolorem saepe rerum tempora asperiores
+                eum neque? Enim, voluptatum! Quo voluptatum necessitatibus tabore esse
+                rat c a t
+            </p>
+        </div>
+    )
+}
+
+export default About
