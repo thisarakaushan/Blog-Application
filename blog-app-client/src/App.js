58579c2447b586091368caa2cab018b72d78f273
thisarakaushan <thisarak9992@gmail.com>
2023-11-11 23:57:46 +0530

    Add files to blog-app-client

--- blog-app-client/src/App.js
+++ blog-app-client/src/App.js
@@ -6,9 +6,13 @@ import About from "./pages/About";
 import ArticlesList from "./pages/ArticlesList";
 import Article from "./pages/Article";
 
+// Components
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <Router>
+      <Navbar />
       <div className="max-w-screen-md mx-auto pt-20">
         <Routes>
           <Route path='/' element={<Home />} />
@@ -16,9 +20,9 @@ function App() {
           <Route path='/article-list' element={<ArticlesList />} />
           <Route path='/article' element={<Article />} />
         </Routes>
-    </div>
+      </div>
     </Router>
-    
+
   );
 }
 
