96071ed535049490b9510969ee948df8d12b7616
thisarakaushan <thisarak9992@gmail.com>
2023-11-12 00:27:52 +0530

    Add files to blog-app-client

--- blog-app-client/src/App.js
+++ blog-app-client/src/App.js
@@ -18,7 +18,7 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
           <Route path='/article-list' element={<ArticlesList />} />
-          <Route path='/article' element={<Article />} />
+          <Route path='/article/:name' element={<Article />} />
         </Routes>
       </div>
     </Router>
